17fad7350f3d1fc582a91c9eb460b7bbbb24c949
keita-yokoo <keita-yokoo@x-power.co.jp>
2018-10-17 17:31:50 +0900

    version_test

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    print(sys.version_info)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ebdjango.settings")
 
     from django.core.management import execute_from_command_line
