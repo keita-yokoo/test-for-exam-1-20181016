5e42ddb0c8d6417f964d01393c103b7002b7f3f1
keita-yokoo <keita-yokoo@x-power.co.jp>
2018-10-24 16:03:06 +0900

    test

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,9 +1,10 @@
  # helloworld/views.py
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import os
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        context = {"test": "OK!"}
+        context = {"test": os.environ["DEBUG"]}
         return render(request, 'index.html', context=context)
