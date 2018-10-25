1ab200b3b737391f60e64bb3b3ab8bfd5f028a0d
keita-yokoo <keita-yokoo@x-power.co.jp>
2018-10-25 10:53:35 +0900

    aws ssm

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,11 +1,43 @@
 from .base import *
 import os
+import boto3
+
+# 設定値リスト
+keys = [
+    "DEBUG",
+    "A_TEST",
+    "TEST"
+]
+
+
+# AWS System Manager パラメータストアから本番環境用設定値を取得する
+def get_params():
+    response = boto3.client("ssm", region_name="us-east-1").get_parameters(
+        Names=keys,
+        WithDecryption=True
+    )
+
+    return {param["Name"]: param["Value"] for param in response["Parameters"]}
+
+
+# パラメータストアから取得した値をグローバル変数に代入しSettingsから参照できるようにする
+parameters = get_params()
+for key in keys:
+    exec("{0}='{1}'".format(key, parameters[key]))
+
+
+# def get_param_test(key, parameters):
+#     for param in parameters:
+#         if param["Name"] == key:
+#             return param["Value"]
+#     raise KeyError(key)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-print(os.environ.keys())
-print(os.environ["DJANGO_SETTINGS_MODULE"])
-DEBUG = os.environ['DEBUG']
+# print(os.environ.keys())
+# DEBUG = get_param("DEBUG")
 
+# print(DEBUG)
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -15,3 +47,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
--- helloworld/views.py
+++ helloworld/views.py
@@ -2,9 +2,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 import os
+from django.conf import settings
 
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        context = {"test": os.environ.get("DEBUG", "NONE!!")}
+        context = {
+            "test": settings.DEBUG,
+            "a": settings.A_TEST,
+            "b": settings.TEST
+        }
         return render(request, 'index.html', context=context)
